a1653b090a4a2e748fe85a3b7b4af78fec3898d9
joedba <joe.gabela@nyu.edu>
2013-03-06 22:59:01 -0500

    Create main.m

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Feb28
+//
+//  Created by Joe Gabela on 2/24/13.
+//  Copyright (c) 2013 Joe Gabela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Feb28AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Feb28AppDelegate class]));
+    }
+}
