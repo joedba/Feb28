e993abb70ee69b21badb9a549cb281c416f076d1
joedba <joe.gabela@nyu.edu>
2013-03-06 22:57:02 -0500

    Create View.h

---
+++ View.h
@@ -0,0 +1,13 @@
+//
+//  View.h
+//  Feb28
+//
+//  Created by Joe Gabela on 2/24/13.
+//  Copyright (c) 2013 Joe Gabela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface View : UIView
+
+@end
